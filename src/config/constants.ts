dfc09b022ae2227e0e523fc36be0e5dc45069346
Jhonatan Rodriguez <jhonathanrodriguez157@gmail.com>
2024-09-16 16:34:10 -0500

    feat: agrega clase para control de errores

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,1 +1,12 @@
 export const MAX_PAGE_SIZE = 50;
+export const REGEX_EMAIL = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+export enum HttpCode {
+	OK = 200,
+	CREATED = 201,
+	NO_CONTENT = 204,
+	BAD_REQUEST = 400,
+	UNAUTHORIZED = 401,
+	FORBIDDEN = 403,
+	NOT_FOUND = 404,
+	INTERNAL_SERVER_ERROR = 500
+}
