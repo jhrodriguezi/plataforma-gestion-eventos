46f6c36e36a734a413583d7e03d076492c884e03
Jhonatan Rodriguez <jhonathanrodriguez157@gmail.com>
2024-09-17 01:41:32 -0500

    refactor: ajusta el errorHandler para manejar los casos conocidos

--- src/presentation/middleware/ErrorHandler.ts
+++ src/presentation/middleware/ErrorHandler.ts
@@ -1,7 +1,20 @@
 import { Request, Response, NextFunction } from "express";
+import { AppError } from "../../errors/customError";
+import { HttpCode } from "../../config/constants";
 
 
-export function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
-    console.error(err.stack)
-    res.status(500).json({ message: "Internal server error" })
+export function errorHandler(error: any, req: Request, res: Response, next: NextFunction) {
+    if (error instanceof AppError) {
+        const { message, name, stack, validationErrors } = error;
+        const statusCode = error.statusCode || HttpCode.INTERNAL_SERVER_ERROR;
+        res.statusCode = statusCode;
+        res.json({ name, message, validationErrors, stack });
+    } else {
+        const name = 'InternalServerError';
+        const message = 'An internal server error occurred';
+        const statusCode = HttpCode.INTERNAL_SERVER_ERROR;
+        res.statusCode = statusCode;
+        res.json({ name, message });
+    }
+    next();
 }
